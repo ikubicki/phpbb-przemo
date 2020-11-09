ded196a9dc4be04a27fdc706282adae6eabbb677
Irek Kubicki <irek@ixdude.com>
2020-11-09 19:30:30 +0100

    Missing BBCodes

--- modules/BBCode/index.js
+++ modules/BBCode/index.js
@@ -30,6 +30,14 @@ bbcode.load([
         out: '<b class="bbcode">$1</b>',
     },
     {
+        in: '\\[u\\](.+?)\\[/u\\]',
+        out: '<u class="bbcode">$1</u>',
+    },
+    {
+        in: '\\[i\\](.+?)\\[/i\\]',
+        out: '<i class="bbcode">$1</i>',
+    },
+    {
         in: '\\[img\\]'+url_expression+'\\[/img\\]',
         out: '<a href="$1" rel="nofollow" data-lightbox="topic-set" target="_blank"><img src="$1" onerror="this.parentElement.replaceWith(\''+bbcode.options.langs.missing_image+'\')" /></a>',
     },
@@ -42,12 +50,20 @@ bbcode.load([
         out: '<a rel="nofollow" href="$1" class="bbcode">$1</a>',
     },
     {
+        in: '\\[colou?r=\\"?([^\\]]+)\\"?\\](.+?)\\[/colou?r\\]',
+        out: '<span style="color:$1" class="bbcode">$2</span>',
+    },
+    {
         in: '\\[(youtube|yt)\\](.+?)((v|embed)(=|/)([a-zA-Z0-9_]+))(.+?)\\[/(youtube|yt)\\]',
         out: '<a href="javascript:void(0)" onclick="bbcode.youtube(this, \'$6\')"  class="youtube"><img src="https://img.youtube.com/vi/$6/0.jpg" /></a>'
 //        out: '<iframe src="https://www.youtube.com/embed/$5" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="video"></iframe>',
     },
     {
         in: '\\[/?[a-z0-9_]+\\]',
+        out: '',
+    },
+    {
+        in: '\\[/?[a-z0-9_]+=([^\\]]+)\\]',
         out: '',
     }
 ])
